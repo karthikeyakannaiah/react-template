9755097831f2b97445ad23f8475e6d49e8610ba2
Karthikeya <karthikeya009k@gmail.com>
2022-09-15 23:30:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import obiwan from './assets/obiwan.png'
+function App() {
+    return (
+        <div className='App'>
+            <img src={obiwan} alt="" />
+            <p>Hello There</p>
+        </div>
+    );
+}
+
+export default App;
